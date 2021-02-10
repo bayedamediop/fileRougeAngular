545f21fa0f1bac289a2c7bcd1e3a37888bb9c8a7
bayedamediop <bayedame1823@gmail.com>
2021-02-10 08:19:43 +0000

    selection les types de image

--- src/app/users/add-user/add-user.component.ts
+++ src/app/users/add-user/add-user.component.ts
@@ -12,8 +12,10 @@ import {Router} from '@angular/router';
 export class AddUserComponent implements OnInit {
   constructor( private service: UserService, private rouet: Router, private formBuilder: FormBuilder) {
   }
-  link = 'assets/images/s2.jpeg';
+  msg = '';
+  link;
   userForm: FormGroup;
+  fakeAuth = false;
   hide = true;
   selectedFile: any;
    user: Users [];
@@ -28,11 +30,17 @@ export class AddUserComponent implements OnInit {
         prenom: ['', [Validators.required]],
         email: ['', [Validators.required, Validators.email]],
         profiles: ['', [Validators.required]],
-        avatar: ['', [Validators.required]],
+        avatar: [''],
     }) ;
   }
   uploadefiler(event): any {
     this.selectedFile =  event.target.files[0];
+    const mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      this.msg = 'Selectionner un image !! Veuillez selectionner un type png/jgeg';
+      return;
+    }
     if (event.target.files){
       const reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
@@ -55,8 +63,10 @@ export class AddUserComponent implements OnInit {
      formData.append('avatar', this.selectedFile);
      this.service.addUser(formData).subscribe(
       (response ) => {
-       alert('User bien ajouter');
-       this.rouet.navigate(['/users']);
+       // alert('User bien ajouter');
+        console.log(response);
+        this.fakeAuth = true ;
+        return ;
       }, (error) => {
         console.log(error);
       }
